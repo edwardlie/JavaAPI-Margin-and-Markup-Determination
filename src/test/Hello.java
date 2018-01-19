c23444ea07760f07dfea1257119b36728aff90fb
Edward <emailedwardlie@gmail.com>
2018-01-18 23:05:29 -0600

    added new files and make project source code more readable

--- src/test/Hello.java
+++ src/test/Hello.java
@@ -10,16 +10,16 @@ import javax.ws.rs.core.Application;
 
 @Path("/hello")
 public class Hello {
-	
-	@GET
+	//says hello in html file
+	/*@GET
 	@Produces(MediaType.TEXT_XML)
 	public String sayhello()
 	{
 		String resource ="<?xml version='1.0'?>" 
 				+ "<hello> Hello from XML</hello>";
 		return resource;
-	}
-	
+	}*/
+	//JSON
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public String sayhelloJSON()
@@ -27,10 +27,10 @@ public class Hello {
 		String resource = null;
 		return resource;
 	}
-	
+	//returns HTML file
 	@GET
 	@Produces(MediaType.TEXT_HTML)
-	public String sayhelloHTML(@QueryParam("name") String name, @QueryParam("revenue") double revenue, @QueryParam("COGS") double cogs){
+	public String calcProfitHTML(@QueryParam("name") String name, @QueryParam("revenue") double revenue, @QueryParam("COGS") double cogs){
 		System.out.println("Name is " + name);
 		System.out.println("Revenue is " + revenue);
 		System.out.println("Cost of Goods Sold is " + cogs);
@@ -45,8 +45,8 @@ public class Hello {
 			
 		System.out.println("How much more you sell your items for than the amount you pay for them");
 			
-		return "<html>" + "<title>" + "->" + name + "</title>"
+		return "<html>" + "<title>" + "Profit information for " + name + "</title>"
 				+"<body><h1>" + "Your margin is " + margin + "%" + " and your markup is " + markup + "%" + "</h1></body>" + "</html>";
 		}
-	//http://localhost:8080/JavaAPI/rest/hello?name=Company&revenue=200&COGS=150
+	//use to test: http://localhost:8080/JavaAPI/rest/hello?name=Company&revenue=200&COGS=150
 }
